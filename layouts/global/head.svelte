ea33066e68340be3b0b081de6af4e8094b0b7e29
Jim Fisk <jim.fisk@jantcu.com>
2025-02-14 18:19:21 -0500

    Fingerprint spa dir.

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -1,5 +1,5 @@
 <script>
-  export let env;
+  export let env, meta;
 </script>
 
 <head>
@@ -7,16 +7,16 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name='viewport' content='width=device-width,initial-scale=1'>
 
-  <title>InnoHub - We're available for marketing</title>
+  <title>{meta?.title}</title>
   
   <base href="{env.baseurl}">
-  <script type="module" src="spa/core/main.js"></script>
+  <script type="module" src={`${env.entrypointJS}/core/main.js`}></script>
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>"
  
   <link rel="icon" type="image/svg+xml" href="favicon.svg">
-  <link rel='stylesheet' href='spa/bundle.css'>
-  <link rel="stylesheet" href="global.css">
+  <link rel='stylesheet' href={`${env.entrypointJS}/bundle.css`}>
+  <link rel='stylesheet' href='global.css?{env.fingerprint}'>
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
